d35e53a3613713f4c7bafc7f8558102fcc18dbe1
Julien <julien.a.laforge@gmail.com>
2021-08-22 17:42:06 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {});
+
+const server = http.createServer(app);
+
+server.listener(3000);
